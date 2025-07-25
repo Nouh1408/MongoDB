95a282e86c05831e3c9aeb61cf5265d0ea9b70c3
Nouh1408 <ahmedinouh@gmail.com>
2025-07-25 08:47:47 +0300

    finnished service to add user and added the middleware

--- src/app.controller.js
+++ src/app.controller.js
@@ -2,6 +2,7 @@ import {connectDB} from './db/connection.js'
 import userRouter from './modules/user.controller.js'
 function bootstrap(app,express){
     connectDB()
+    app.use(express.json())
     app.use('/user',userRouter)
 }
 export default bootstrap
--- src/modules/user.service.js
+++ src/modules/user.service.js
@@ -1,8 +1,15 @@
 import { User } from "../db/models/user.model.js"
 
 export const createUser = async (req,res)=>{
-    //get data from request, check for existence, create user
+    try {
+        //get data from request, check for existence, create user
    const userExist = await User.findOne({email:req.body.email})
+   if(userExist){ throw new Error("this user already exist",{cause:409})}
+   const {insertedId}=await User.insertOne(req.body)
+   return res.status(201).json({message:"user created successfully", success:true, data:{insertedId}})
+    } catch (error) {
+        return res.status(error.cause || 500).json({message:error.message, success:false, stack:error.stack})
+    }
 }
 export const getAllUSers=(req,res)=>{
 
